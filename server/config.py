56dcad35232e72f6c65de4ca33b724367191e50c
GiftXXVI <gift.chimphonda@outlook.com>
2022-07-15 16:39:38 +0200

    Updated env

--- server/config.py
+++ server/config.py
@@ -8,9 +8,12 @@ def get_config() -> dict:
     db_pass = getenv('DB_PASSWD')
     db_host = getenv('DB_HOST')
     db_port = getenv('DB_PORT')
+    app_env = getenv('APP_ENV')
     db_credentials = f'{db_user}:{db_pass}'
     db_socket = f'{db_host}:{db_port}'
     conf["SQLALCHEMY_DATABASE_URI"] = f'postgresql://{db_credentials}@{db_socket}/{db_name}'
+    if app_env == 'CONTAINER':
+        conf["SQLALCHEMY_DATABASE_URI"] = f'sqlite://'
     conf["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     conf['SECRET_KEY'] = getenv('APP_KEY')
     return conf
